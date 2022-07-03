f4bcd771f8f6cd6fc422aa9a0bc9ea6c928137cd
Charan-Kumaran <107575167+Charan-Kumaran@users.noreply.github.com>
2022-07-03 19:51:24 +0530

    Add files via upload

---
+++ printing +ve nos in range(mycaptain).py
@@ -0,0 +1,9 @@
+#printing positive numbers in a range
+list1=[12,-7,5,64,-14]
+for numbers in list1:
+    if numbers>0:
+        print(numbers)
+list2=[12,14,-95,3]
+for numbers in list2:
+    if numbers>0:
+        print(numbers)        
