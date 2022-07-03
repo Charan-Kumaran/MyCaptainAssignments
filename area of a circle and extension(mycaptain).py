df452059a95f88d82503fe6328bc97b6c8eb8259
Charan-Kumaran <107575167+Charan-Kumaran@users.noreply.github.com>
2022-07-03 19:29:01 +0530

    Add files via upload

---
+++ area of a circle and extension(mycaptain).py
@@ -0,0 +1,11 @@
+#area of a circle using python
+r=int(input("Enter thr radius of circle in cm"))
+area=3.14*r**2
+print(area)
+
+#extension of file using python
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
+
+
